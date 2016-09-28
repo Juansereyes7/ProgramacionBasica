06d412811304a00c40016f0f6120e4647fefc688
Juan Reyes <juansefreak@gmail.com>
2016-09-27 22:05:03 -0500

    Se invierte el eje de inicio para generar el dibujo de la torre

--- Dibujo_canvas/torre.js
+++ Dibujo_canvas/torre.js
@@ -2,16 +2,13 @@ alert ("comando prueba a ver si funciona");
 var d = document.getElementById("torre");
 var lienzo = d.getContext("2d");
 var i =0;
-
-
 while (i<=300)
 {
   lienzo.beginPath();
   lienzo.strokeStyle = "red";
-  lienzo.moveTo (0,i);
+  lienzo.moveTo (i,0);
   i = i + 10;
-  lienzo.lineTo (i,300);
+  lienzo.lineTo (300,i);
   lienzo.stroke();
   lienzo.closePath();
-
 }
