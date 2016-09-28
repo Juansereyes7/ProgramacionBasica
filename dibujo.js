497d554bbd1c56cef5f2e3008cf9075b22b029a1
Juan Reyes <juansefreak@gmail.com>
2016-09-27 21:03:52 -0500

    se dibja x con canvas sin ayuda del tutor

--- dibujo.js
+++ dibujo.js
@@ -1,9 +1,11 @@
-alert ("funciona!!")
+alert ("Funciona, dibujando con canvas");
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 lienzo.beginPath();
 lienzo.strokeStyle = "red";
-lienzo.moveTo (100,100);
-lienzo.lineTo (200,200);
+lienzo.moveTo(100,100);
+lienzo.lineTo(200,200);
+lienzo.moveTo(200,100);
+lienzo.lineTo (100,200);
 lienzo.stroke();
 lienzo.closePath();
