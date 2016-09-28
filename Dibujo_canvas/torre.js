97ec5734adeb2298da980e01a6091453bd26cca8
Juan Reyes <juansefreak@gmail.com>
2016-09-27 21:52:24 -0500

    Se realiza el ejercicio de clase sin ayuda del tutor, queda muy bien

--- Dibujo_canvas/torre.js
+++ Dibujo_canvas/torre.js
@@ -1,1 +1,17 @@
 alert ("comando prueba a ver si funciona");
+var d = document.getElementById("torre");
+var lienzo = d.getContext("2d");
+var i =0;
+
+
+while (i<=300)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = "red";
+  lienzo.moveTo (0,i);
+  i = i + 10;
+  lienzo.lineTo (i,300);
+  lienzo.stroke();
+  lienzo.closePath();
+
+}
