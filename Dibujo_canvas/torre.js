84568130b785c662568cf9b52bf62e8b9620875f
Juan Reyes <juansefreak@gmail.com>
2016-10-01 12:46:29 -0500

    Se crea etiqueta input y se llama como evento en el java script

--- Dibujo_canvas/torre.js
+++ Dibujo_canvas/torre.js
@@ -1,5 +1,8 @@
 alert ("comando prueba a ver si funciona");
 var d = document.getElementById("torre");
+var texo = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener ("click",dibujoPorClick);
 var lienzo = d.getContext("2d");
 var i =0;
 while (i<=300)
@@ -11,4 +14,9 @@ while (i<=300)
   lienzo.lineTo (300,i);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick ()
+{
+  alert("No me toque ahi");
 }
