f87d4733be2bfd0f545d4ce15fe02c6317cfd07f
Juan Reyes <juansefreak@gmail.com>
2016-10-01 13:24:35 -0500

    se identifican las etiquetas por parte del HTML input para realizar formularios, y se toma la informacion en el javaScript por medio del escuchador de eventos addEventListener

--- Dibujo_canvas/torre.js
+++ Dibujo_canvas/torre.js
@@ -1,22 +1,23 @@
 alert ("comando prueba a ver si funciona");
 var d = document.getElementById("torre");
-var texo = document.getElementById("texto_lineas");
+var texto = document.getElementById("texto_lineas");
 var boton = document.getElementById("botoncito");
 boton.addEventListener ("click",dibujoPorClick);
 var lienzo = d.getContext("2d");
 var i =0;
-while (i<=300)
-{
-  lienzo.beginPath();
-  lienzo.strokeStyle = "red";
-  lienzo.moveTo (i,0);
-  i = i + 10;
-  lienzo.lineTo (300,i);
-  lienzo.stroke();
-  lienzo.closePath();
-}
-
 function dibujoPorClick ()
 {
-  alert("No me toque ahi");
+  console.log(texto);
+  var t = parseInt(texto.value);
+  alert("No me toque ahi " + t);
+  while (i<=t)
+  {
+    lienzo.beginPath();
+    lienzo.strokeStyle = "red";
+    lienzo.moveTo (i,0);
+    i = i + 10;
+    lienzo.lineTo (300,i);
+    lienzo.stroke();
+    lienzo.closePath();
+  }
 }
