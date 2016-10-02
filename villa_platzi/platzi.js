1f60516d3d5783490d1645f66259ff056999d24f
Juan Reyes <juansefreak@gmail.com>
2016-10-02 13:30:06 -0500

    se modifican archivos sin llamar imagenes al js

--- villa_platzi/platzi.js
+++ villa_platzi/platzi.js
@@ -1,1 +1,12 @@
-alert ("funciona");
+var x = parseInt (prompt ("ingresa el minimo"));
+var y = parseInt(prompt ("ingresa el máximo"));
+var z = aleatorio (x, y);
+
+function aleatorio (min, max)
+{
+  var resultado;
+  resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+  return resultado;
+}
+
+document.write (parseInt(z));
